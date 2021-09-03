c41a48f4f45569e7417a7220067e10b65f03aa33
terry <terryoyty@163.com>
2021-09-03 13:38:17 +0800

    迁移腾讯云

--- Blog/settings/production.py
+++ Blog/settings/production.py
@@ -31,5 +31,5 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'www.terryoyty.online']
-ALLOWED_HOSTS = ['www.terryoyty.online']
+# ALLOWED_HOSTS = ['127.0.0.1', 'www.terryoyty.com']
+ALLOWED_HOSTS = ['www.terryoyty.com']
